e24b70a497576efaa5c069f7814173ec2dddc223
B00144945 <B00144945@mytudublin.ie>
2023-10-04 10:27:30 +0100

    Added the Loop in fives

--- phpForLoop.php
+++ phpForLoop.php
@@ -4,7 +4,7 @@
 #put a descripition of what the code does here - 
 #this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
 for ($i=1;$i<=20;$i++) {
-    echo "$i, <br> ";
+    echo "$i, ";
 }
 
 ?> 
